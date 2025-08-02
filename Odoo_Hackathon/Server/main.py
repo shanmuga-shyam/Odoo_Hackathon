a47e581cd286d7399b68d5148f5389207a7a7886
Shyam <shanmugashyam0606@gmail.com>
2025-08-02 16:57:22 +0530

    frontend + backend

--- Odoo_Hackathon/Server/main.py
+++ Odoo_Hackathon/Server/main.py
@@ -3,7 +3,18 @@ from app.database import Base, engine
 from app.routes import auth, issues
 
 Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or restrict to your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 app.include_router(auth.router, prefix="/api", tags=["Auth"])
 app.include_router(issues.router, prefix="/api", tags=["Issues"])
